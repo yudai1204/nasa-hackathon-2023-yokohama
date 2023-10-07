d79d8aad8200f4857829871a505131e88a462487
Horike Haruki <igagrid407@gmail.com>
2023-10-07 12:05:07 +0900

    [Add] moon

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,26 +1,15 @@
 import { Box } from "@chakra-ui/react";
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Header } from "./header";
+import { MainCanvas } from "./mainCanvas";
 
 export const Main = () => {
   return (
-    <Box w="100vw" h="100vh">
+    <>
       <Header />
-      <Canvas
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 1000,
-          position: [0, 0, 5],
-        }}
-        style={{ background: "black" }}
-      >
-        <directionalLight position={[1, 1, 1]} intensity={0.8} />
-        <ambientLight args={[0xffffff]} intensity={0.5} />
-        <OrbitControls minDistance={15} maxDistance={70} />
-      </Canvas>
-    </Box>
+      <Box w="100vw" h="100vh">
+        <MainCanvas />
+      </Box>
+    </>
   );
 };
---
+++ src/components/mainCanvas.tsx
@@ -0,0 +1,22 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Moon } from "./moon";
+
+export const MainCanvas = () => {
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 1000,
+        position: [0, 0, 5],
+      }}
+      style={{ background: "black" }}
+    >
+      <directionalLight position={[1, 1, 1]} intensity={0.8} />
+      <ambientLight args={[0xffffff]} intensity={0.5} />
+      <OrbitControls minDistance={15} maxDistance={70} />
+      <Moon />
+    </Canvas>
+  );
+};
