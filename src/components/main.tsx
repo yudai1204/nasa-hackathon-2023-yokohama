ef70ac361f4f24d1268e9c7f574e797a18e5486d
Horike Haruki <igagrid407@gmail.com>
2023-10-07 10:58:43 +0900

    [Add] Canvas

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,11 +1,26 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
 import { Header } from "./header";
 
 export const Main = () => {
   return (
-    <Box>
+    <Box w="100vw" h="100vh">
       <Header />
-      <Text>Main Page</Text>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 1000,
+          position: [0, 0, 5],
+        }}
+        style={{ background: "black" }}
+      >
+        <directionalLight position={[1, 1, 1]} intensity={0.8} />
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <OrbitControls minDistance={15} maxDistance={70} />
+      </Canvas>
     </Box>
   );
 };
