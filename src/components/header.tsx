c924439e58857cfd5e1d245e5207bc32e0c88772
miuye <fallioss13@outlook.jp>
2023-10-07 22:46:56 +0900

    add header
    header途中

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,35 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Icon, useDisclosure, SlideFade, VStack, Text } from "@chakra-ui/react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export const Header = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Box>
-      <Text>Header</Text>
+    <Box position="absolute" top={0} left={0} userSelect="none" h="fit-content">
+      <Icon
+        position="absolute"
+        top="24px"
+        left="24px"
+        as={isOpen ? AiOutlineClose : AiOutlineMenu}
+        fontSize={24}
+        color={isOpen ? "gray.600" : "gray.300"}
+        onClick={onToggle}
+        zIndex={1}
+      />
+      <SlideFade in={isOpen} offsetX={-20} offsetY={0} unmountOnExit>
+        <VStack bgColor="gray.300" h="100vh" pt="72px" pl="26px" gap={2} color="gray.600">
+          <Box w="100%">
+            <Text fontSize={24} fontWeight="medium">
+              Filter
+            </Text>
+          </Box>
+          <Box w="100%">
+            <Text fontSize={24} fontWeight="medium">
+              Settings
+            </Text>
+          </Box>
+          <Box w="100%">Change Size</Box>
+        </VStack>
+      </SlideFade>
     </Box>
   );
 };
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -7,7 +7,7 @@ import { MapComponent } from "./mapLibre";
 export const Main = () => {
   const [isMap, setIsMap] = useState(true);
   return (
-    <Box w="100%" h="100%" position="relative">
+    <Box w="100%" h="100vh" position="relative" overflow="none">
       <Header />
       {/* 仮の切り替えボタン */}
       <Button
