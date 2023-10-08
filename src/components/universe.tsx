4c4b6f32aa264cf611f0d127c059a36ec7aa2604
joraku daijiro <daijiro20020502@gmail.com>
2023-10-08 11:30:05 +0900

    fix setBackground

--- src/components/universe.tsx
+++ src/components/universe.tsx
@@ -1,17 +1,13 @@
 import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import * as THREE from "three";
-
+import { BackSide, TextureLoader } from "three";
 export const Universe = () => {
   const radius = 500;
   const universeMap = useLoader(TextureLoader, "universe.jpg");
 
   return (
-    <group>
-      <mesh>
-        <sphereGeometry args={[radius, 128, 64]} />
-        <meshBasicMaterial side={THREE.BackSide} map={universeMap} />
-      </mesh>
-    </group>
+    <mesh>
+      <sphereGeometry args={[radius, 128, 64]} />
+      <meshBasicMaterial side={BackSide} map={universeMap} />
+    </mesh>
   );
 };
