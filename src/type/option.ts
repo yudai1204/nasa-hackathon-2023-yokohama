e59360358eeb2598a95a78e3ed0fff4997d684c0
Horike Haruki <igagrid407@gmail.com>
2023-10-08 17:20:37 +0900

    [Add] impl auto-rotate

--- src/type/option.ts
+++ src/type/option.ts
@@ -1,9 +1,11 @@
 export type Option = {
+  autoRotate: boolean;
   minYear: number;
   maxYear: number;
 };
 
 export class OptionConstants {
+  static readonly autoRotate = true;
   static readonly minYear = 1965;
   static readonly maxYear = 1980;
 }
