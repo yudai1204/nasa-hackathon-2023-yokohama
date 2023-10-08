28110e3a1910328607148a4a6c91a7ee0602df34
yudai hiyama <yudai1204h@gmail.com>
2023-10-08 12:06:40 +0900

    change zindex

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -22,10 +22,10 @@ export const Main = () => {
         {isMap ? "toThree" : "toMap"}
       </Button>
 
-      <Box w="100%" h="100%" position="absolute" top={0} left={0} zIndex={isMap ? 1 : 0}>
+      <Box w="100%" h="100%" position="absolute" top={0} left={0} zIndex={isMap ? 0 : -1}>
         <MapComponent setIsMap={setIsMap} />
       </Box>
-      <Box w="100%" h="100%" position="absolute" top={0} left={0} zIndex={isMap ? 0 : 1}>
+      <Box w="100%" h="100%" position="absolute" top={0} left={0} zIndex={isMap ? -1 : 0}>
         <MainCanvas />
       </Box>
     </Box>
