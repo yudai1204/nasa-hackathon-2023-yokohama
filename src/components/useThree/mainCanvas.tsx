ca16c5b89a4d0f46342387b3444f3f51e7f5d0f9
Horike Haruki <igagrid407@gmail.com>
2023-10-10 00:57:13 +0900

    ごめんなさい

--- src/components/useThree/mainCanvas.tsx
+++ src/components/useThree/mainCanvas.tsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
 import { Moon } from "./moon";
 import { Universe } from "./universe";
 import type { MoonquakeData } from "@/type";
@@ -16,19 +17,34 @@ export const MainCanvas = (props: Props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { moonquakeData, setIsMap, option, choiceMoonquake, setChoiceMoonquake } = props;
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const orbitControlsRef = useRef<any>(null);
+
   return (
     <Canvas
       camera={{
         fov: 45,
         near: 0.1,
         far: 1000,
-        position: [0, 0, 0],
+        position: [0, 300, 0],
       }}
       style={{ background: "black" }}
     >
       <directionalLight position={[1, 1, 1]} intensity={0.8} />
       <ambientLight args={[0xffffff]} intensity={0.5} />
-      <OrbitControls minDistance={250} maxDistance={500} enablePan={false} />
+      <OrbitControls
+        ref={orbitControlsRef}
+        minDistance={150}
+        maxDistance={500}
+        enablePan={false}
+        onChange={() => {
+          const orbitControls = orbitControlsRef.current;
+          if (!orbitControls) return;
+          if (orbitControls.getDistance() == orbitControls.minDistance) {
+            setIsMap(true);
+          }
+        }}
+      />
       <Moon
         option={option}
         moonquakeData={moonquakeData}
