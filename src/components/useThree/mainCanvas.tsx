766d20964e60dc4278d437cadbe5f4694963ba54
Horike Haruki <igagrid407@gmail.com>
2023-10-10 17:14:54 +0900

    [Fix]

--- src/components/useThree/mainCanvas.tsx
+++ src/components/useThree/mainCanvas.tsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
+import type { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 import { Moon } from "./moon";
 import { Universe } from "./universe";
 import type { MoonquakeData } from "@/type";
