d41ca79ade13dde719ff65242750806a527fc718
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2023-10-12 12:55:50 +0900

    [refactor] filter

--- src/utils/filterMoonquake.ts
+++ src/utils/filterMoonquake.ts
@@ -1,19 +1,20 @@
 import { MoonquakeData, Option, OptionConstants } from "@/type";
 
 export const filterMoonQuake = (moonquakeData: MoonquakeData[], option: Option): MoonquakeData[] => {
+  const typeFilteredmoonquakeData = moonquakeData.filter((moonquake) => option.typeFilter.has(moonquake.type));
+
   if (option.playInfo.status === "stop") {
-    const isAll = option.minYear === OptionConstants.minYear && option.maxYear === OptionConstants.maxYear;
-    return moonquakeData
-      .filter((moonquake) => {
-        if (isAll) return true;
-        const year = moonquake.time?.year ?? 0;
-        return year >= option.minYear && year <= option.maxYear;
-      })
-      .filter((moonquake) => option.typeFilter.has(moonquake.type));
+    if (option.minYear === OptionConstants.minYear && option.maxYear === OptionConstants.maxYear) {
+      return typeFilteredmoonquakeData;
+    }
+
+    return typeFilteredmoonquakeData.filter((moonquake) => {
+      const year = moonquake.time?.year ?? 0;
+      return year >= option.minYear && year <= option.maxYear;
+    });
   }
-  return moonquakeData
-    .filter((moonquake) => {
-      return moonquake.time?.year === option.playInfo.year;
-    })
-    .filter((moonquake) => option.typeFilter.has(moonquake.type));
+
+  return typeFilteredmoonquakeData.filter((moonquake) => {
+    return moonquake.time?.year === option.playInfo.year;
+  });
 };
