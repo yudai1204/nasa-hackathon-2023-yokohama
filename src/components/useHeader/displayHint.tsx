be031095f09b2094fe0d4af97516f25969fae915
joraku daijiro <daijiro20020502@gmail.com>
2023-10-14 04:05:13 +0900

    [add] create-modal

--- src/components/useHeader/displayHint.tsx
+++ src/components/useHeader/displayHint.tsx
@@ -1,4 +1,54 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  Icon,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import { AiFillClockCircle, AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { ExplainBox } from "./explainBox";
+
+const manipulations = [
+  {
+    subtitle: "2D Map",
+    children: "Scroll up and zoom to switch to 2D map.",
+  },
+  {
+    subtitle: "Filter",
+    children: "Select the types of moonquakes that display.",
+  },
+  {
+    subtitle: "General Settings",
+    children:
+      "Performance Mode is literally a mode that improves performance. Auto Rotate allows you to select whether the moon rotates or not.",
+  },
+  {
+    subtitle: "Time Speed",
+    children: "Adjust the playback speed.",
+  },
+  {
+    subtitle: "Layer Select",
+    children: "Select the layers of the 2D map and adjust its opacity.",
+  },
+  {
+    subtitle: "Playback",
+    children: (
+      <>
+        Manual playback is possible by operating the slide bar. If you want to play automatically, click this icon
+        <Icon as={AiFillClockCircle} /> and press the play button <Icon as={AiFillPlayCircle} />. Press this icon
+        <Icon as={AiFillPauseCircle} /> to stop playback. Adjust the playback speed using Time Speed.
+      </>
+    ),
+  },
+  {
+    subtitle: "Detail",
+    children: "Show details about earthquake.",
+  },
+];
 
 type Props = {
   onClose: () => void;
@@ -8,16 +58,44 @@ type Props = {
 export const DisplayHintModal = (props: Props) => {
   const { onClose, isOpen } = props;
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size={{ base: "full", md: "xl" }}>
       <ModalOverlay />
       <ModalContent>
         <ModalCloseButton />
-        <ModalHeader>How to use</ModalHeader>
+        <ModalHeader style={{ fontSize: "33px" }}>Guide</ModalHeader>
         <ModalBody>
-          This application displays the moonquakes observed by Apollo. You can scroll, zoom in, and manipulate the moon.
-          In the left sidebar, you can customize the functions to your liking, and in the right Details section, you can
-          see detailed information about the moonquakes. You can also click the clock icon in the bottom bar to play the
-          moonquakes in chronological order.
+          <Box>
+            <Text fontWeight="bold" fontSize="140%">
+              Overview
+            </Text>
+            <Box pl="20px" pr="20px" pt="10px" pb="10px">
+              <Text>
+                This application displays moonquakes observed by Apollo, and by combining them with other data, you can
+                examine the mechanism of it.
+              </Text>
+              <iframe
+                src="https://www.youtube.com/embed/Il3rgj9Lo7g?si=MMDTfpaB14lkOs4B"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                style={{ aspectRatio: "16/9", width: "100%" }}
+              />
+            </Box>
+          </Box>
+
+          <Box>
+            <Text fontWeight="bold" fontSize="140%">
+              Methods of operation
+            </Text>
+            <Box pl="20px" pr="20px" pt="10px" pb="10px">
+              {manipulations.map((manipulation, idx) => (
+                <ExplainBox key={idx} subtitle={manipulation.subtitle}>
+                  {manipulation.children}
+                </ExplainBox>
+              ))}
+            </Box>
+          </Box>
         </ModalBody>
       </ModalContent>
     </Modal>
