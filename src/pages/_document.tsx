fb4f0c19aa1b0a3d19955868fc26ee12c3f538dd
yudai <yudai1204u@gmail.com>
2023-10-16 21:36:44 +0900

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#603cba" />
       </Head>
       <body style={{ overflow: "hidden" }}>
         <Main />
